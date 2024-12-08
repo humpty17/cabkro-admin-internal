7deb131e0c0077633937adbfaaf71690cd444e8f
Suyash Srivastava <suyashsrivastava200@gmail.com>
2024-12-08 13:27:46 +0530

    asterik field add

--- src/General/Label/FormLabel.jsx
+++ src/General/Label/FormLabel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FormLabel = ({label}) => {
   return (
-    <label className="col-form-label col-sm-3 text-sm-end">{label}</label>
+    <label className="col-form-label col-sm-3 text-sm-end required">{label}</label>
   )
 }
 
