253f2d5e6292f044aaccf728294f6d7a1f3fd1ce
himanshigoel17 <himanshigoel1705@gmail.com>
2024-12-15 22:15:43 +0530

    Update GeneralMethod.js

--- src/General/GeneralMethod.js
+++ src/General/GeneralMethod.js
@@ -15,3 +15,17 @@ export const callApi = async (methodType, url, apiData, apiHeader) =>{
         return null
     }
 }
+
+
+export const getCurrentDateTime = () => {
+    const today = new Date();
+
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0"); // Month is 0-indexed
+    const day = String(today.getDate()).padStart(2, "0");
+    const hours = String(today.getHours()).padStart(2, "0");
+    const minutes = String(today.getMinutes()).padStart(2, "0");
+    const seconds = String(today.getSeconds()).padStart(2, "0"); // Add seconds
+
+    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`; // Include seconds
+}
