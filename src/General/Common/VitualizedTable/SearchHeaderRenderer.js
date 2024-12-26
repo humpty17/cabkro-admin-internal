9b08034c5e0cffcd11e1e2d872ec10a54d973818
Suyash Srivastava <suyashsrivastava200@gmail.com>
2024-12-26 22:19:01 +0530

    css update

--- src/General/Common/VitualizedTable/SearchHeaderRenderer.js
+++ src/General/Common/VitualizedTable/SearchHeaderRenderer.js
@@ -3,13 +3,21 @@ import { ACTION } from "../../ConstStates";
 export const headerRenderer = ({ dataKey, label, tableSearchFilters, handleFilterChange }) => {
   return (
     <div>
-      <div className="ReactVirtualized__Table__headerTruncatedText">{label}</div>
-      {label!==ACTION?<input
-        type="text"
-        placeholder={`Search ${label}`}
-        value={tableSearchFilters[dataKey] || ''} // Ensure no undefined value
-        //onChange={(e) => handleFilterChange(dataKey, e.target.value)}
-        style={{ width: "90%", padding: "4px", marginTop: "1px" }}
-      /> : <div></div>}
-    </div> )
+      <div className="ReactVirtualized__Table__headerTruncatedText">
+        {label}
+      </div>
+      {label !== ACTION ? (
+        <input
+          type="text"
+          placeholder={`Search ${label}`}
+          className="search"
+          value={tableSearchFilters[dataKey] || ""} // Ensure no undefined value
+          //onChange={(e) => handleFilterChange(dataKey, e.target.value)}
+          // style={{ width: "90%", padding: "4px", marginTop: "1px" }}
+        />
+      ) : (
+        <div style={{ marginBottom: "28px" }}></div>
+      )}
+    </div>
+  );
 };
--- src/pages/Data/PopularDestinations.jsx
+++ src/pages/Data/PopularDestinations.jsx
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import { FaFileExport, FaTrash } from "react-icons/fa";
-import { FiPlus, FiDownload } from "react-icons/fi";
+import { FiPlus, FiDownload, FiTrash2 } from "react-icons/fi";
 import { AutoSizer, Column, Table } from "react-virtualized";
 import "react-virtualized/styles.css";
 import { LoadingContext } from "../../store/loading-context";
 import { callApi, getCurrentDateTime } from "../../General/GeneralMethod";
 import { NotificationManager } from "react-notifications";
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
 import { headerRenderer } from "../../General/Common/VitualizedTable/SearchHeaderRenderer";
 import UploadExcelButton from "../../General/Buttons/UploadExcelButton";
 import DownloadExcelButton from "../../General/Buttons/DownloadExcelButton";
@@ -14,118 +14,127 @@ import SubmitExcelButton from "../../General/Buttons/SubmitExcelButton";
 import CancelExcelButton from "../../General/Buttons/CancelExcelButton";
 import { LoginContext } from "../../store/login-context";
 import VirtualizedTable from "../../General/Common/VitualizedTable/VirtualizedTable";
-import { ACTION, APICALLFAIL, APINULLERROR, DELETEDATAERROR } from "../../General/ConstStates";
-
+import {
+  ACTION,
+  APICALLFAIL,
+  APINULLERROR,
+  DELETEDATAERROR,
+} from "../../General/ConstStates";
 
 const PopularDestinations = () => {
   const columns = [
-      {
-        label: "PackageName",
-        dataKey: "packageName",
-        width: 300,
-      },
-      {
-        label: "Description",
-        dataKey: "description",
-        width: 300,
-      },
-      {
-        label: "pickupCity",
-        dataKey: "pickupCity",
-        width: 150,
-      },
-      {
-        label: "destinationCity",
-        dataKey: "destinationCity",
-        width: 250,
-      },
-      {
-        label: "vehicleType",
-        dataKey: "vehicleType",
-        width: 250,
-      },
-      {
-        label: ACTION,
-        dataKey: ACTION,
-        width: 150,
-        cellRenderer: ({ rowData }) => (
-          <div>
-            <FaTrash
-              style={{ cursor: "pointer", color: "red" }}
-              onClick={() => handleDeleteDestination(rowData)}
-            />
-          </div>
-        ),
-      },
-    ];
-  const {user} = useContext(LoginContext)
+    {
+      label: "PackageName",
+      dataKey: "packageName",
+      width: 300,
+    },
+    {
+      label: "Description",
+      dataKey: "description",
+      width: 300,
+    },
+    {
+      label: "pickupCity",
+      dataKey: "pickupCity",
+      width: 150,
+    },
+    {
+      label: "destinationCity",
+      dataKey: "destinationCity",
+      width: 250,
+    },
+    {
+      label: "vehicleType",
+      dataKey: "vehicleType",
+      width: 250,
+    },
+    {
+      label: ACTION,
+      dataKey: ACTION,
+      width: 150,
+      cellRenderer: ({ rowData }) => (
+        <div>
+          <FiTrash2
+            style={{ cursor: "pointer", color: "red" }}
+            onClick={() => handleDeleteDestination(rowData)}
+          />
+        </div>
+      ),
+    },
+  ];
+  const { user } = useContext(LoginContext);
   const popularDataExcelHeader = {
-    "packageName": "",
-    "description": "",
-    "pickupCity": "",
-    "destinationCity": "",
-    "minDistance": 0,
-    "maxDistance": 0,
-    "basePrice": 0,
-    "discountRate": 0,
-    "discountAmount": 0,
-    "vehicleType": "",
-    "vehicleFuelType": "",
-    "vehicleModelName": "",
-    "vehicleSeaterCount": 0,
-    "pickupAddress": "",
-    "destinationAddress": "",
-    "pickupLatLong": 0,
-    "destinationLatLong": 0,
-    "timeDurationHours": 0,
-    "dayCount": 0,
-    "specificDay": 0,
-    "specificDate": 0,
-    "offerPrice": 0,
-    "rentalHours": 0,
-    "rentaldays": 0,
-    "plusMember": 0,
-    "gstRate": 0,
-    "gstAmount": 0,
-    "offerDescription": "",
-    "breakFast": true,
-    "lunch": true,
-    "dinner": true,
-    "extraService": "",
+    packageName: "",
+    description: "",
+    pickupCity: "",
+    destinationCity: "",
+    minDistance: 0,
+    maxDistance: 0,
+    basePrice: 0,
+    discountRate: 0,
+    discountAmount: 0,
+    vehicleType: "",
+    vehicleFuelType: "",
+    vehicleModelName: "",
+    vehicleSeaterCount: 0,
+    pickupAddress: "",
+    destinationAddress: "",
+    pickupLatLong: 0,
+    destinationLatLong: 0,
+    timeDurationHours: 0,
+    dayCount: 0,
+    specificDay: 0,
+    specificDate: 0,
+    offerPrice: 0,
+    rentalHours: 0,
+    rentaldays: 0,
+    plusMember: 0,
+    gstRate: 0,
+    gstAmount: 0,
+    offerDescription: "",
+    breakFast: true,
+    lunch: true,
+    dinner: true,
+    extraService: "",
     // "createdDate": "2024-12-24T12:56:26.270Z",
     // "modifyDate": "2024-12-24T12:56:26.270Z",
     // "userId": 0,
     // "isActive": true,
     // "isDeleted": true,
-    "other1": 0,
-    "other2": ""
-  }
+    other1: 0,
+    other2: "",
+  };
 
   const otherData = {
-      "createdDate": getCurrentDateTime(),
-      "modifyDate": getCurrentDateTime(),
-      "userId": user ? user.userId : 0,
-      "isActive": true,
-      "isDeleted": false,
-    }
-  
-  const {startLoading, stopLoading} = useContext(LoadingContext)
-  const [getDestination, setGetDestination] = useState([])
-  const [searchFilters, setSearchFilters] = useState('')
-  const [isShowPreview, setIsShowPreview] = useState(false)
-  const [previewBookingData, setPreviewBookingData] = useState([])
-  const rowGetter = ({ index }) => isShowPreview ? previewBookingData[index] : getDestination[index];
+    createdDate: getCurrentDateTime(),
+    modifyDate: getCurrentDateTime(),
+    userId: user ? user.userId : 0,
+    isActive: true,
+    isDeleted: false,
+  };
+
+  const { startLoading, stopLoading } = useContext(LoadingContext);
+  const [getDestination, setGetDestination] = useState([]);
+  const [searchFilters, setSearchFilters] = useState("");
+  const [isShowPreview, setIsShowPreview] = useState(false);
+  const [previewBookingData, setPreviewBookingData] = useState([]);
+  const rowGetter = ({ index }) =>
+    isShowPreview ? previewBookingData[index] : getDestination[index];
 
   const getPopularDestination = async () => {
     startLoading();
     try {
-      const response = await callApi("get",`${process.env.REACT_APP_API_URL_ADMIN}Data/GetAllPopularDestinations`,{},{});
+      const response = await callApi(
+        "get",
+        `${process.env.REACT_APP_API_URL_ADMIN}Data/GetAllPopularDestinations`,
+        {},
+        {}
+      );
       stopLoading();
       if (response !== null && response !== undefined) {
         if (response.data.code === 200) {
-          setGetDestination(response.data.data)
+          setGetDestination(response.data.data);
           console.log(response);
-          
         } else {
           NotificationManager.error(response.data.message);
         }
@@ -138,89 +147,89 @@ const PopularDestinations = () => {
     }
   };
 
-  useEffect(() =>{
-    getPopularDestination()
-  },[])
+  useEffect(() => {
+    getPopularDestination();
+  }, []);
 
-  const submitExcelData = async ()=>{
-      if(previewBookingData.length === 0){
-        NotificationManager.warning("No data available for upload.")
-        return
-      }
-      startLoading()
-      try{
-        const response = await callApi("post",`${process.env.REACT_APP_API_URL_ADMIN}Data/AddOrUpdatePopularDestinations`,previewBookingData,{});
-        stopLoading();
-        if (response !== null && response !== undefined) {
-          if (response.data.code === 200) {
-           NotificationManager.success(response.data.message)
-           handleReset()
-          } else {
-            NotificationManager.error(response.data.message);
-          }
-        } else {
-          console.error("API returned an invalid response:", response);
-          NotificationManager.warning(response.data.message);
-        }
-      }
-      catch(err){
-        stopLoading()
-      }
+  const submitExcelData = async () => {
+    if (previewBookingData.length === 0) {
+      NotificationManager.warning("No data available for upload.");
+      return;
     }
-
-     const handleDeleteDestination = async (rowData) => {
-        const { packageID } = rowData;
-
-        startLoading();
-        try {
-          //debugger
-          const response = await callApi(
-            "DELETE",
-            `${process.env.REACT_APP_API_URL_ADMIN}Data/DeletePopularDestination/${packageID}`,
-            {},
-            {}
-          );
-          // console.log(response);
-    
-          stopLoading();
-          if(response!==null && response!==undefined){
-            if (response?.data?.code === 200) {
-              NotificationManager.success(
-                response?.data?.message || "Destination deleted successfully"
-              );
-              getPopularDestination()
-            } else {
-              NotificationManager.error(
-                response?.data?.message || DELETEDATAERROR
-              );
-            }
-          }
-          else{
-            NotificationManager.error(APINULLERROR)
-          }
-          
-        } catch (error) {
-          stopLoading();
-          console.error(APICALLFAIL, error);
-          NotificationManager.error(APICALLFAIL,error)
+    startLoading();
+    try {
+      const response = await callApi(
+        "post",
+        `${process.env.REACT_APP_API_URL_ADMIN}Data/AddOrUpdatePopularDestinations`,
+        previewBookingData,
+        {}
+      );
+      stopLoading();
+      if (response !== null && response !== undefined) {
+        if (response.data.code === 200) {
+          NotificationManager.success(response.data.message);
+          handleReset();
+        } else {
+          NotificationManager.error(response.data.message);
         }
-      };
+      } else {
+        console.error("API returned an invalid response:", response);
+        NotificationManager.warning(response.data.message);
+      }
+    } catch (err) {
+      stopLoading();
+    }
+  };
 
-  const setPreviewData = (data)=>{
-    setIsShowPreview(true)
-    setPreviewBookingData(data)
-  }
+  const handleDeleteDestination = async (rowData) => {
+    const { packageID } = rowData;
 
-  const handleCancelClick = ()=>{
-    setIsShowPreview(false)
-    setPreviewBookingData(false)
-  }
+    startLoading();
+    try {
+      //debugger
+      const response = await callApi(
+        "DELETE",
+        `${process.env.REACT_APP_API_URL_ADMIN}Data/DeletePopularDestination/${packageID}`,
+        {},
+        {}
+      );
+      // console.log(response);
 
-  const handleReset = ()=>{
-    setIsShowPreview(false)
-    setPreviewBookingData([])
-    getPopularDestination()
-  }
+      stopLoading();
+      if (response !== null && response !== undefined) {
+        if (response?.data?.code === 200) {
+          NotificationManager.success(
+            response?.data?.message || "Destination deleted successfully"
+          );
+          getPopularDestination();
+        } else {
+          NotificationManager.error(response?.data?.message || DELETEDATAERROR);
+        }
+      } else {
+        NotificationManager.error(APINULLERROR);
+      }
+    } catch (error) {
+      stopLoading();
+      console.error(APICALLFAIL, error);
+      NotificationManager.error(APICALLFAIL, error);
+    }
+  };
+
+  const setPreviewData = (data) => {
+    setIsShowPreview(true);
+    setPreviewBookingData(data);
+  };
+
+  const handleCancelClick = () => {
+    setIsShowPreview(false);
+    setPreviewBookingData(false);
+  };
+
+  const handleReset = () => {
+    setIsShowPreview(false);
+    setPreviewBookingData([]);
+    getPopularDestination();
+  };
 
   return (
     <div className="wrapper">
@@ -231,8 +240,9 @@ const PopularDestinations = () => {
             <div className="row">
               <div className="col-12">
                 <div className="card">
-                <div className="card-header row">
-                    <h2 className="col-5">{isShowPreview ? "Preview" : ""}</h2>
+                  <div className="card-header ">
+                    <div className="row">
+                    <h2 className="col-5 font">{isShowPreview ? "Preview" : ""}</h2>
                     <div className="mb-3 text-end col-7">
                       {isShowPreview === false ? (
                         <UploadExcelButton
@@ -248,7 +258,7 @@ const PopularDestinations = () => {
                       ) : null}
                       {isShowPreview === true ? (
                         <SubmitExcelButton
-                         handleSubmitClick={submitExcelData}
+                          handleSubmitClick={submitExcelData}
                         ></SubmitExcelButton>
                       ) : null}
                       {isShowPreview === true ? (
@@ -257,13 +267,22 @@ const PopularDestinations = () => {
                         ></CancelExcelButton>
                       ) : null}
                     </div>
+                    </div>
                   </div>
                   <div className="card-body">
                     <div className="dataTables_wrapper dt-bootstrap5 no-footer">
-                      
                       <div className="row dt-row">
                         <div className="col-sm-12">
-                        <VirtualizedTable tableData={isShowPreview ? previewBookingData : getDestination} tableSearchFilters={searchFilters} columns={columns} rowGetter={rowGetter}/>
+                          <VirtualizedTable
+                            tableData={
+                              isShowPreview
+                                ? previewBookingData
+                                : getDestination
+                            }
+                            tableSearchFilters={searchFilters}
+                            columns={columns}
+                            rowGetter={rowGetter}
+                          />
                         </div>
                       </div>
                     </div>
